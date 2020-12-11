7209a7855399cde32b4fc86c1ec211aaf1413c9b
dylan171717 <72532563+dylan171717@users.noreply.github.com>
2020-12-10 19:59:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodstock
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
